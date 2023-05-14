89070eb8360ed64fd53f88f0e4d16621785e347e
Semkiv Andriy <semkivandriy@gmail.com>
2023-05-14 11:46:49 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UtilityPaymentManager.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
